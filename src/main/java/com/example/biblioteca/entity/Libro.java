e961e7867b707715e9b6d6fe53f2b93f04f335df
DavidJaramillo1 <rdavidja02@educastur.es>
2025-10-24 20:51:21 +0200

    ultimo comit lombok

--- src/main/java/com/example/biblioteca/entity/Libro.java
+++ src/main/java/com/example/biblioteca/entity/Libro.java
@@ -1,5 +1,20 @@
 package com.example.biblioteca.entity;
 
+@Data // Evita definir los getters y setters
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "libro")
 public class Libro {
+
+    @Id
+    @GenerateValue(strategy = GenerationType.IDENTITY) // Se genera de forma automatica
+    private Long id;
+
+    @Column(nullable = false) // Al ponerlo posterior, hayq que modificar la propiedad
+    private String titulo;
+
+    @Column
+    private String autor;
 
 }
